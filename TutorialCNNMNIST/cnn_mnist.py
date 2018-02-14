ec56c471e1bd7551fb3cd917074b9395cb25ceb8
Dani <thef1dani@gmail.com>
2018-02-14 13:33:14 +0100

    Update cnn_mnist.py

--- TutorialCNNMNIST/cnn_mnist.py
+++ TutorialCNNMNIST/cnn_mnist.py
@@ -1,18 +1,3 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -39,6 +24,7 @@ def main(unused_argv):
   mnist_classifier = tf.estimator.Estimator(
       model_fn=model.cnn_model_fn, model_dir="mnist_convnet_model")
 
+  # Comentadas estas lineas para ejecutar solo el modo PREDICT
   # Set up logging for predictions
   # Log the values in the "Softmax" tensor with label "probabilities"
   # tensors_to_log = {"probabilities": "softmax_tensor"}
